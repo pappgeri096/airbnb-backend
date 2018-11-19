4cb5948a15662529ded2a32f35593eec5b2291d6
attilaKincsei <akincsei@gmail.com>
2018-11-19 10:20:14 +0100

    initial commit

---
+++ src/main/java/com/codecool/airbnbmanager/AirbnbManagerApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.airbnbmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbnbManagerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/codecool/airbnbmanager/AirbnbManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecool.airbnbmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AirbnbManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
