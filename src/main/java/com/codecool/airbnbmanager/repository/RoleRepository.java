912599c89673b298eea266b656dc27df71d4e693
pappgeri096 <pappgergo096@gmail.com>
2019-02-07 13:29:08 +0100

    basic jwt auth copied from other project

---
+++ src/main/java/com/codecool/airbnbmanager/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.codecool.airbnbmanager.repository;
+
+import com.codecool.airbnbmanager.model.Role;
+import com.codecool.airbnbmanager.util.enums.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
