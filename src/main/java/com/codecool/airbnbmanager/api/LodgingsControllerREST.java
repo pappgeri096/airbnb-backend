805a51e014abe3563667923e9a0d28c0daaf87ca
pappgeri096 <pappgergo096@gmail.com>
2019-02-18 10:51:31 +0100

    added roles

--- src/main/java/com/codecool/airbnbmanager/api/LodgingsControllerREST.java
+++ src/main/java/com/codecool/airbnbmanager/api/LodgingsControllerREST.java
@@ -11,6 +11,7 @@ import com.codecool.airbnbmanager.util.enums.LodgingsType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.WebDataBinder;
@@ -33,11 +34,13 @@ public class LodgingsControllerREST {
     UserServiceREST userServiceREST;
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('USER') OR hasRole('PROPERTY') OR hasRole('LANDLORD')")
     public Lodgings getLodgingsById(@PathVariable("id") long id){
         return lodgingsServiceREST.getLodgingsById(id);
     }
 
     @PostMapping("/{username}/add")
+    @PreAuthorize("hasRole('LANDLORD')")
     public ResponseEntity<?> addNewLodgings(@RequestBody Lodgings lodgings, @PathVariable("username") String username ){
 
         System.out.println(lodgings.getLodgingsType());
@@ -45,11 +48,6 @@ public class LodgingsControllerREST {
         lodgings.setLandlord(user);
         lodgingsServiceREST.addNewLodgings(lodgings);
         return new ResponseEntity<>(new ResponseMessage("Lodgings added succesfully!"), HttpStatus.OK);
-    }
-
-    @InitBinder
-    public void initBinder(WebDataBinder dataBinder) {
-        dataBinder.registerCustomEditor(LodgingsType.class, new LodginsTypeConverter());
     }
 
 }
