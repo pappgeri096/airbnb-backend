54c403cf313765552faf54a44c64f1b7ffb27dc9
pappgeri096 <pappgergo096@gmail.com>
2019-02-18 14:35:37 +0100

    updating logings works

--- src/main/java/com/codecool/airbnbmanager/api/LodgingsControllerREST.java
+++ src/main/java/com/codecool/airbnbmanager/api/LodgingsControllerREST.java
@@ -50,14 +50,26 @@ public class LodgingsControllerREST {
         return new ResponseEntity<>(new ResponseMessage("Lodgings added successfully!"), HttpStatus.OK);
     }
 
-    @PutMapping("/{username}/update")
+    @PutMapping("/{id}/update")
     @PreAuthorize("hasRole('LANDLORD')")
-    public ResponseEntity<?> updateLodgings(@RequestBody Lodgings lodgings, @PathVariable("username") String username ){
+    public ResponseEntity<?> updateLodgings(@RequestBody Lodgings lodgings, @PathVariable("id") long id ){
 
-        System.out.println(lodgings.getLodgingsType());
-        User user = userServiceREST.getUserByUsername(username);
-        lodgings.setLandlord(user);
-        lodgingsServiceREST.updateLodgings(lodgings);
+        Lodgings currentLodgings = lodgingsServiceREST.getLodgingsById(id);
+        if(currentLodgings == null){
+            return new ResponseEntity("NOT FOUND",
+                    HttpStatus.NOT_FOUND);
+        }
+
+        currentLodgings.setName(lodgings.getName());
+        currentLodgings.setLodgingsType(lodgings.getLodgingsType());
+        currentLodgings.setPricePerDay(lodgings.getPricePerDay());
+        currentLodgings.setElectricityBill(lodgings.getElectricityBill());
+        currentLodgings.setGasBill(lodgings.getGasBill());
+        currentLodgings.setTelecommunicationBill(lodgings.getTelecommunicationBill());
+        currentLodgings.setCleaningCost(lodgings.getCleaningCost());
+        currentLodgings.setFullAddress(lodgings.getFullAddress());
+
+        lodgingsServiceREST.updateLodgings(currentLodgings);
         return new ResponseEntity<>(new ResponseMessage("Lodgings updated successfully!"), HttpStatus.OK);
     }
 
