794eb7265f08b15172fb29f21dca46f306d0d214
pappgeri096 <pappgergo096@gmail.com>
2019-02-21 10:51:04 +0100

    adding new todo works

--- src/main/java/com/codecool/airbnbmanager/api/ToDoControllerREST.java
+++ src/main/java/com/codecool/airbnbmanager/api/ToDoControllerREST.java
@@ -1,14 +1,31 @@
 package com.codecool.airbnbmanager.api;
 
+import com.codecool.airbnbmanager.message.response.ResponseMessage;
+import com.codecool.airbnbmanager.model.ToDo;
 import com.codecool.airbnbmanager.service.ToDoServiceREST;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/todos")
 public class ToDoControllerREST {
 
     @Autowired
     private ToDoServiceREST toDoService;
+
+    @PostMapping("/{lodgingsId}/add")
+    @PreAuthorize("hasRole('USER') OR hasRole('LANDLORD')")
+    public ResponseEntity<?> addNewTodo(@PathVariable("lodgingsId") long lodgingsId, @RequestBody ToDo toDo){
+
+        if(!toDoService.addNewTodo(lodgingsId, toDo)){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(new ResponseMessage("ADDED"), HttpStatus.OK);
+
+    }
 }
