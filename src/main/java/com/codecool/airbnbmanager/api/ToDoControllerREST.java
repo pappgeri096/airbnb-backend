3fde14b3516ea25705dfd88a8c77b94811117633
pappgeri096 <pappgergo096@gmail.com>
2019-02-23 10:47:32 +0100

    response messages refactored in todos controller

--- src/main/java/com/codecool/airbnbmanager/api/ToDoControllerREST.java
+++ src/main/java/com/codecool/airbnbmanager/api/ToDoControllerREST.java
@@ -1,5 +1,6 @@
 package com.codecool.airbnbmanager.api;
 
+import com.codecool.airbnbmanager.exceptions.LodgingsNotFoundException;
 import com.codecool.airbnbmanager.model.Lodgings;
 import com.codecool.airbnbmanager.model.ToDo;
 import com.codecool.airbnbmanager.repository.LodgingsRepository;
@@ -7,6 +8,9 @@ import com.codecool.airbnbmanager.repository.ToDoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -22,8 +26,8 @@ public class ToDoControllerREST {
     @PostMapping("/{lodgingsId}")
     @PreAuthorize("hasRole('USER') OR hasRole('LANDLORD')")
     public ToDo addNewTodo(@PathVariable("lodgingsId") long lodgingsId, @RequestBody ToDo toDo){
-        Lodgings lodgings = lodgingsRepository.findById(lodgingsId).orElse(null);
-        if(lodgings==null) return null;
+        Lodgings lodgings = lodgingsRepository.findById(lodgingsId)
+                .orElseThrow(() -> new LodgingsNotFoundException(lodgingsId));
         toDo.setLodgings(lodgings);
         return toDoRepository.save(toDo);
 
@@ -31,10 +35,12 @@ public class ToDoControllerREST {
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('USER') OR hasRole('LANDLORD')")
-    public void addNewTodo(@PathVariable("id") long id){
+    public Map<String, Boolean> addNewTodo(@PathVariable("id") long id){
         ToDo toDo = toDoRepository.findById(id).orElse(null);
-        if(toDo==null) return;
         toDoRepository.delete(toDo);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
 
     }
 }
