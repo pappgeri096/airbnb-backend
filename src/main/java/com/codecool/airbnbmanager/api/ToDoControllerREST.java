8c99d86e5f03da43d3e35d6de7cc547f2c4e837d
pappgeri096 <pappgergo096@gmail.com>
2019-03-04 10:44:02 +0100

    Todos marking as done implemented

--- src/main/java/com/codecool/airbnbmanager/api/ToDoControllerREST.java
+++ src/main/java/com/codecool/airbnbmanager/api/ToDoControllerREST.java
@@ -6,7 +6,9 @@ import com.codecool.airbnbmanager.model.Lodgings;
 import com.codecool.airbnbmanager.model.ToDo;
 import com.codecool.airbnbmanager.repository.LodgingsRepository;
 import com.codecool.airbnbmanager.repository.ToDoRepository;
+import com.codecool.airbnbmanager.util.enums.ToDoStatusType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -41,6 +43,16 @@ public class ToDoControllerREST {
         ToDo toDo = toDoRepository.findById(id)
                 .orElseThrow(() -> new ToDoNotFoundException(id));
         toDoRepository.delete(toDo);
+        return ResponseEntity.ok().body(Boolean.TRUE);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('USER') OR hasRole('LANDLORD')")
+    public ResponseEntity<Boolean> markTodo(@PathVariable("id") long id, @RequestBody ToDo todo){
+        ToDo currentTodo = toDoRepository.findById(todo.getId())
+                .orElseThrow(() -> new ToDoNotFoundException(todo.getId()) );
+        currentTodo.setStatus(ToDoStatusType.DONE);
+        toDoRepository.save(currentTodo);
         return ResponseEntity.ok().body(Boolean.TRUE);
     }
 }
