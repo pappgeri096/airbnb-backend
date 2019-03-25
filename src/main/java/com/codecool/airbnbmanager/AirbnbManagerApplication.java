1c54de695b897a0f36e6e10fefd7e8085a8044f4
pappgeri096 <pappgergo096@gmail.com>
2019-03-25 19:19:29 +0100

    fix

--- src/main/java/com/codecool/airbnbmanager/AirbnbManagerApplication.java
+++ src/main/java/com/codecool/airbnbmanager/AirbnbManagerApplication.java
@@ -1,7 +1,6 @@
 package com.codecool.airbnbmanager;
 
 import com.codecool.airbnbmanager.repository.UserRepository;
-import com.codecool.airbnbmanager.service.UserServiceREST;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
