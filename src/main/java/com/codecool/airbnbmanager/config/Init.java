2ca8489231ccd4a88bd74a24716f25c9b94a146f
pappgeri096 <pappgergo096@gmail.com>
2019-04-05 10:07:57 +0200

    updated init

--- src/main/java/com/codecool/airbnbmanager/config/Init.java
+++ src/main/java/com/codecool/airbnbmanager/config/Init.java
@@ -34,6 +34,47 @@ public class Init implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        Address address2 = new Address("Hungary", "Budapest", "1065", "Nagymező u. 44");
+        Address address4 = new Address("Hungary", "Miskolc", "3525", "Régiposta u. 9.");
+        User user = new User("kedvesMiki", "What", "Who!", "kedvesmiki@gmail.com", "+36306000000",
+                encoder.encode("12345678"));
+
+        User user2 = new User("vidisBali", "Farago", "Balazs", "vidisbali@gmail.com", "+36306000001",
+                encoder.encode("12345678"));
+        Set<Role> roles = new HashSet<>();
+        Set<Role> roles2 = new HashSet<>();
+
         roleRepository.save(new Role(RoleName.ROLE_USER));
+
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+
+        Role landlordRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+        roles.add(landlordRole);
+        roles2.add(userRole);
+
+        user2.setRoles(roles);
+        user.setRoles(roles2);
+        userRepository.save(user);
+        userRepository.save(user2);
+
+        Lodgings lodgings = new Lodgings("CodeCool Budapest", LodgingsType.FAMILY_HOUSE, 250000, 30000, 10000, 10000, 25000, user,
+                address2);
+        lodgings.setTenants(user);
+        lodgingsRepository.save(lodgings);
+
+        Lodgings lodgings2 = new Lodgings("CodeCool Miskolc", LodgingsType.FAMILY_HOUSE, 1000, 444, 555, 666, 6666, user,
+                address4);
+        lodgings2.setTenants(user2);
+        lodgingsRepository.save(lodgings2);
+
+        ToDo toDo = new ToDo("ELtort az ablak", lodgings, new Date(),"Az egyik idi.. diak 'veletlenul' eltorte. ", 7000);
+        ToDo toDo2 = new ToDo("Nem mukodik a lift", lodgings, new Date(),"Nem mukodik.", 10000);
+        ToDo toDo3 = new ToDo("Nem lehet lehuzni a wct", lodgings, new Date(),"Szag...", 10000);
+        toDoRepository.save(toDo);
+        toDoRepository.save(toDo2);
+        toDoRepository.save(toDo3);
+
     }
 }
